ce2cd81d0965af6d481e62c3dde4d62170a786ed
pinkadotted <subkmrspm@gmail.com>
2023-05-13 01:38:44 +0800

    added typerwriter effect

--- src/index.js
+++ src/index.js
@@ -3,4 +3,23 @@ import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
+// initTiltEffect();
+
+
+
+import Typewriter from 'typewriter-effect/dist/core';
+
+new Typewriter('#typewriter', {
+    strings: [' Hi, I\'m <span class="color-lavendar">Subesh</span> <br /> Welcome to my page &#x1F44B'],
+    autoStart: true,
+    loop: false,
+    stop: true
+});
+
+// var app = document.getElementById('typer');
+
+// var typewriter = new Typewriter(app, {
+//   loop: true
+// });
+
+// typewriter.start();
