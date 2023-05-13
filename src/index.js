2e9604be67cfffcedd76038aeedfa8d8a89c2bd9
pinkadotted <subkmrspm@gmail.com>
2023-05-13 23:28:51 +0800

    fixed typewriter, added cta button and animations

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import TypeIt from "typeit";
 
 initScrollReveal(targetElements, defaultProps);
-// initTiltEffect();
 
+// new TypeIt("#typewriter", {
+//     speed: 80,
+//     startDelay: 900,
+// }).type('').go();
 
-
-import Typewriter from 'typewriter-effect/dist/core';
-
-new Typewriter('#typewriter', {
-    strings: [' Hi, I\'m <span class="color-lavendar">Subesh</span> <br /> Welcome to my page &#x1F44B'],
-    autoStart: true,
-    loop: false,
-    stop: true
-});
-
-// var app = document.getElementById('typer');
-
-// var typewriter = new Typewriter(app, {
-//   loop: true
-// });
-
-// typewriter.start();
+new TypeIt("#typewriter", {
+    speed: 80,
+    startDelay: 2500,
+}).type('').go();
